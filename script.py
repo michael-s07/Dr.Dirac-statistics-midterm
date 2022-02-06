c76e0147401c6fee1fc7e5109cdd483ca3577d46
Michael S"exit()quit()git config --global user.name Michael <michael.sosu@amalitech.org>
2022-02-06 00:28:13 +0000

    Solution Commit

---
+++ script.py
@@ -0,0 +1,14 @@
+#P(A|B) = P(knows the material | answers correctly)
+
+#P(A) = P(knows the material) = 0.6
+#P(B) = P(answers correctly)
+
+#P(B|A) = P(answers correctly|knows the material) = 0.85
+
+#P(answers correctly | knows the material) * P(knows the material) OR (+)
+#P(answers correctly | does not know the material) * P(does not know the material)
+
+# P(A|B) = P(B|A) * P(A) / P(B)
+print( 0.85 * 0.6 / 0.59)
+
+# The Probability is 86% Chance
